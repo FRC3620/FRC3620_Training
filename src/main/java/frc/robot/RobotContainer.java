c36088e4e10af81d99c2173e37490e5220b844a4
Desmond <10641@stjoebears.com>
2023-11-28 19:31:38 -0500

    lesson 3

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,7 @@ public class RobotContainer {
   // hardware here...
 
   // subsystems here
-
+public static PropellorSubsystem propellorSubsystem;
   // joysticks here....
   public static Joystick driverJoystick;
   public static Joystick operatorJoystick;
@@ -43,6 +43,7 @@ public class RobotContainer {
   }
 
   private void makeSubsystems() {
+    propellorSubsystem = new PropellorSubsystem();
   }
 
   /**
@@ -60,6 +61,7 @@ public class RobotContainer {
 
   private void setupSmartDashboardCommands() {
     // DriveSubsystem
+    SmartDashboard.putData(new RunPropellorCommand());
   }
 
   SendableChooser<CommandFactory> chooser = new SendableChooser<>();
